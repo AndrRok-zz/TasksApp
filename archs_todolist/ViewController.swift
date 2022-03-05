a9f8832d7144eb695d58b2b98d20c76847269c8e
AndrRok <rokhmanov2000@yandex.ru>
2022-03-05 18:57:37 +0400

    Initial Commit

---
+++ archs_todolist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  archs_todolist
+//
+//  Created by Andrey Rokhmanov on 05.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
